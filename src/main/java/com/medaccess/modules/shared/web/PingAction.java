ab95794b6458b7972003651271e24c6c5ee3c9b1
Eder Garcia <ey16_619@hotmail.com>
2026-01-11 08:00:14 +0000

    First commit
    Stable base structure of this project

---
+++ src/main/java/com/medaccess/modules/shared/web/PingAction.java
@@ -0,0 +1,11 @@
+package com.medaccess.modules.shared.web;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class PingAction extends ActionSupport {
+
+    @Override
+    public String execute() {
+        return SUCCESS;
+    }
+}
---
+++ src/main/webapp/ping.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ping II</title>
+</head>
+<body>
+<h1>MedAccess EMR - OK</h1>
+</body>
+</html>
